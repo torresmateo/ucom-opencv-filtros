8554064ab26a833cd51d863f0d37e195fc963ecd
Mateo Torres <torresmateo@gmail.com>
2020-08-17 21:06:38 -0300

    mejor geometria

--- proyecto/filtro.py
+++ proyecto/filtro.py
@@ -7,27 +7,41 @@ from utils import agregar_imagen
 class Filtro(object):
     
     def __init__(self,
-                 imagen, 
-                 offset_ancho, 
-                 offset_x,
-                 offset_y):
+                 imagen):
         self.imagen = imageio.imread(imagen)
-        self.offset_ancho = offset_ancho
-        self.offset_x = offset_x
-        self.offset_y = offset_y
-        
+        self.cantidad_canales = (self.imagen.shape[-1]
+                                 if len(self.imagen.shape) > 2 else 2)
+        if self.cantidad_canales > 2:
+            mascara2d = self.imagen.sum(axis=-1)
+        else:
+            mascara2d = self.imagen.copy()
+        suma_c = mascara2d.sum(axis=0)
+        suma_f = mascara2d.sum(axis=1)
+        pos_i_x = (suma_c != 0).tolist().index(True)
+        pos_f_x = (suma_c.shape[0] - (suma_c != 0)[::-1].tolist().index(True))
+        pos_i_y = (suma_f != 0).tolist().index(True)
+        pos_f_y = (suma_f.shape[0] - (suma_f != 0)[::-1].tolist().index(True))
+        if self.cantidad_canales > 2:
+            self.imagen = self.imagen[pos_i_y:pos_f_y, pos_i_x:pos_f_x, :]
+        else:
+            self.imagen = self.imagen[pos_i_y:pos_f_y, pos_i_x:pos_f_x]
+        self.h, self.w = self.imagen.shape[:2]
+        self.aspect_ratio = self.w / self.h
+
     def agregar_a_imagen(self, img, x, y, w, h):
         
-        # TODO: mejorar la geometria del escalado
-        factor_de_resize = (w+self.offset_ancho)/self.imagen.shape[0]
-        
-        # coordenada del punto a alinear en el nuevo tamaño
-        offset_x_resize = int(factor_de_resize * self.offset_x)
-        offset_y_resize = int(factor_de_resize * self.offset_y)
-        
-        imagen_chica = cv.resize(self.imagen, 
-                                 (w + self.offset_ancho, 
-                                  w + self.offset_ancho))
-        agregar_imagen(img, imagen_chica, 
-                       x - offset_x_resize, 
-                       y - offset_y_resize)
+        # ajustar el alto
+        w_h = int(self.aspect_ratio * h)
+        a_h = w_h * h
+        # ajustar el ancho
+        h_w = int(w / self.aspect_ratio)
+        a_w = w * h_w 
+        # tomar el ajuste que genere la mayor area
+        if a_h > a_w:
+            rw, rh = (w_h, h)
+            rx, ry = x - np.abs(int((w - rw)/2)), y
+        else:
+            rw, rh = (w, h_w)
+            rx, ry = x, y - np.abs(int((h - rh)/2))
+        imagen_chica = cv.resize(self.imagen, (rw, rh))
+        agregar_imagen(img, imagen_chica, rx, ry)
--- proyecto/webcam.py
+++ proyecto/webcam.py
@@ -10,21 +10,21 @@ image_dir = os.path.join(script_dir, 'imagenes')
 mascara = os.path.join(image_dir,"stormtrooper-ojos-transparentes.png")
 mascara2 = os.path.join(image_dir,"stormtrooper.png")
 
-filtros = [Filtro(mascara, 100, 115, 105), 
-           Filtro(mascara2, 100, 115, 105),
-           Filtro(mascara2, 100, 115, 105)]
+filtros = [Filtro(mascara), 
+           Filtro(mascara2),
+           Filtro(mascara2)]
 
 haar_cascade_file = os.path.join(script_dir, 
                                  'modelos', 
                                  'haarcascade_frontalface_default.xml')
 detector_caras = cv.CascadeClassifier(haar_cascade_file)
 
-
-
-        
-
 # capturar imagen desde la webcam
 cap = cv.VideoCapture(0)
+
+# PARAMETROS PARA AJUSTAR EL BORDE DE LA CARA AL BORDE DE LA CABEZA
+AGREGAR_X_PORCENTAJE = 0.05
+AGREGAR_Y_PORCENTAJE = 0.15
 
 while True:
     ret, img = cap.read() # leer la webcam
@@ -41,14 +41,46 @@ while True:
     # if len(caras) > 1: # se detectaron múltiples caras
     #    caras = caras[caras[0,:].argsort()]
     
+
+    
     for i, (x, y, w, h) in enumerate(caras):
-        filtros[i].agregar_a_imagen(img, x, y, w, h)
-        cv.rectangle(img,(x,y), (x+w, y+h), (255,0,0))
+        
+        pad_w = int(w * AGREGAR_X_PORCENTAJE)
+        pad_h = int(h * AGREGAR_Y_PORCENTAJE)
+        
+        x_pad = x - pad_w
+        y_pad = y - pad_h
+        w_pad = w + 2*pad_w
+        h_pad = h + 2*pad_h
+
+        filtros[i].agregar_a_imagen(img, x_pad, y_pad, w_pad, h_pad)
+        cv.rectangle(img,(x,y), (x+w, y+h), (255,0,0)) # la cara detectada
+
+        # el rectangulo con margen
+        cv.rectangle(img,(x_pad,y_pad), (x_pad+w_pad, y_pad+h_pad), (0,0,255))
+    cv.putText(img,
+               f'AGREGAR_X_PORCENTAJE: {AGREGAR_X_PORCENTAJE}',
+               (20,30), 
+               cv.FONT_HERSHEY_COMPLEX,
+               0.71, 
+               (0,0,255),
+               2)
+    cv.putText(img,
+               f'AGREGAR_Y_PORCENTAJE: {AGREGAR_Y_PORCENTAJE}',
+               (20,50), 
+               cv.FONT_HERSHEY_COMPLEX,
+               0.71, 
+               (0,0,255),
+               2)
     
     cv.imshow('Ttulo de la ventana', img)
 
     k = cv.waitKey(30)
     if k == 27: # ESC (ASCII)
         break
+    elif k == ord('y'):
+        AGREGAR_Y_PORCENTAJE += 0.01
+    elif k == ord('u'):
+        AGREGAR_Y_PORCENTAJE -= 0.01
 cap.release()
 cv.destroyAllWindows()
